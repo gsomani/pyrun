e17457790250a74b77d2af49189773d1cc08af5e
Gaurav Somani <gauravsomani@iisc.ac.in>
2021-12-06 18:02:26 +0530

    update

--- add_cell.js
+++ add_cell.js
@@ -4,5 +4,4 @@ function add_code_cell(div)
   ele.setAttribute("data-executable", "true");
   ele.setAttribute("data-language", "python");
   document.getElementById(div).appendChild(ele);
-  thebe.bootstrap();
 }
