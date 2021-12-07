8c5d7e238d90f2a2c5f726904ad87b2734e8227e
Gaurav Somani <gauravsomani@iisc.ac.in>
2021-12-07 10:27:23 +0530

    update

---
+++ _includes/py/test.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def int_to_bit_array(num,bits): 
+    bit = np.empty(bits,dtype=int) 
+    for i in range(bits): 
+        bit[i] = num & 1 
+        num = num >> 1 
+    return bit
+
+def bits_to_int(bits):
+    num = 0
+    for i in range(len(bits)):
+        num += 1 << bits[i] ;
+    return num
+
+def disparity(num,bits): 
+    return 2*num.bit_count() - bits
+
+def xored(num,bits): 
+    bit = int_to_bit_array(num,bits) 
+    x = np.empty(bits+1,dtype=int) 
+    x[0] = bit[0] 
+    for i in range(1,bits): 
+        x[i] = x[i-1] ^ bit[i] 
+    x[bits]=1
+    x = bits_to_int(x) 
+    return x 
+
+def data_word(num):
+    ones = num.bit_count()
+    x = int(xored(num,8))
+    y = x ^ 0b010101011
+    if(ones<4 or (ones==4 and (num&1))):
+        return x
+    else:
+        return y
+        
+
+for i in range(256):
+    print(disparity(data_word(i),9))    
